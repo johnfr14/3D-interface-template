9c9769781d65d2f38af73208af3eced5985662fc
John <bioj@hotmail.fr>
2022-10-15 16:10:58 +0700

    update

--- src/Lib/interfaces.ts
+++
@@ -1,6 +0,0 @@
-export const interfaces = {
-  kek: ["function getSample(uint) external"],
-  alpha: ["function getSample(uint) external"],
-  fomo: ["function getSample(uint) external"],
-  fud: ["function getSample(uint) external"]
-}
