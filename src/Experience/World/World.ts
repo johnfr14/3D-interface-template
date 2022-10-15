adbbc658d89ed67f1d15417fe80d5c02b8238bcb
John <johnfr14.dev@proton.me>
2022-10-15 11:50:27 +0700

    World: switch to typescript

---
+++ src/Experience/World/World.ts
@@ -0,0 +1,38 @@
+import * as THREE from "three"
+import Experience from "../Experience";
+import Environment from "./Environment";
+import Floor from "./Floor";
+import User from "./User";
+import Resources from "../Utils/Resources";
+
+export default class World {
+  // Classes
+  experience: Experience
+  scene: THREE.Scene
+  resources: Resources
+  floor?: Floor
+  user?: User
+  environment?: Environment
+
+
+  constructor() {
+
+    this.experience = Experience.Instance();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+
+
+    this.resources.on("ready", () => {
+      this.floor = new Floor();
+      this.user = new User();
+      this.environment = new Environment();
+    })
+
+  }
+
+  update(): void 
+  {
+    if (this.user) { this.user.update() }
+  }
+
+}
